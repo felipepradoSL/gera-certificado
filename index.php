7594cf542beb33bd3f17c71574abecd00152bba6
felipepradoSL <felipe@sweetleads.com.br>
2020-09-04 08:52:55 -0300

    funcional Catlen Guerra por popup

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
-/**
+
+	/**
 	* Plugin Name: Gera Certificado SL
 	* Plugin URI: https://www.swetleads.com.br
+	* Description: Utilizar o shortcode [certificado-form]
 	* Version: 1.0
 	* Author: Sweet Leads
 	* Author URI: https://www.swetleads.com.br
@@ -10,93 +12,97 @@
 	setlocale( LC_ALL, 'pt_BR', 'pt_BR.iso-8859-1', 'pt_BR.utf-8', 'portuguese' );
 	date_default_timezone_set( 'America/Sao_Paulo' );
 	require('fpdf/alphapdf.php');
-	require('PHPMailer/class.phpmailer.php');	
-	
-
+	require('PHPMailer/class.phpmailer.php');
 
 	function gera_certificado(){ 
-
-				// Formulario HTML
 		?>
+		<!-- Formulario HTML -->
 		<form class="form-horizontal" action="" method="post"  id="contact_form_quiz">
 			<fieldset>
-				<center>
-					<h1>Quiz da Catlen</h1>
-				</center>
 				<p>&nbsp;</p>				
 				<div class="form-group">
-					<label class="col-md-4 control-label" >Pergunta</label> 
+					<label class="col-md-4 control-label" >Palavra Chave 1</label> 
 					<div class="col-md-4 inputGroupContainer">
 						<div class="input-group">
 							<span class="input-group-addon"><i class="glyphicon glyphicon-barcode"></i></span>
-							<input name="resposta1" placeholder="Resposta" class="form-control"  type="text">
+							<input name="resposta1" placeholder="Resposta" class="form-control"  type="text" required>
 						</div>
 					</div>
 				</div>
 				<div class="form-group">
-					<label class="col-md-4 control-label" >Pergunta</label> 
+					<label class="col-md-4 control-label" >Palavra Chave 2</label> 
 					<div class="col-md-4 inputGroupContainer">
 						<div class="input-group">
 							<span class="input-group-addon"><i class="glyphicon glyphicon-barcode"></i></span>
-							<input name="resposta2" placeholder="Resposta" class="form-control"  type="text">
+							<input name="resposta2" placeholder="Resposta" class="form-control"  type="text" required>
 						</div>
 					</div>
 				</div>
 				<div class="form-group">
-					<label class="col-md-4 control-label" >Pergunta</label> 
+					<label class="col-md-4 control-label" >Palavra Chave 3</label> 
 					<div class="col-md-4 inputGroupContainer">
 						<div class="input-group">
 							<span class="input-group-addon"><i class="glyphicon glyphicon-barcode"></i></span>
-							<input name="resposta3" placeholder="Resposta" class="form-control"  type="text">
+							<input name="resposta3" placeholder="Resposta" class="form-control"  type="text" required>
 						</div>
 					</div>
 				</div>
 				<div class="form-group">
 					<label class="col-md-4 control-label"></label>
 					<div class="col-md-4">
-						<button type="submit" name="btn-quiz" class="btn btn-default col-md-12" >Enviar Respostas </button>
+						<button name="btn-quiz" class="btn btn-default col-md-12" >Enviar Respostas </button>
 					</div>
 				</div>
 			</fieldset>
-			<div id="msg_contact_certificate"></div>
 		</form>
 
+
 		<?php 
-
+		
+		// Se o botão de enviar respostas for pressionado
+		//	as respostas serão verificadas de acordo com os inputs
 		if (isset($_POST['btn-quiz'])) {
-			$resposta1 = $_POST["resposta1"];
-			$resposta2 = $_POST["resposta2"];
-			$resposta3 = $_POST["resposta3"];
+			//palavras chaves para as respostas do quiz
+			$keyresposta1 = "foco";
+			$keyresposta2 = "maquiagem";
+			$keyresposta3 = "empresaria";
 
+			$acertos = 0; //contador de acertos
 
-			$keys = array("resposta1", "resposta2", "resposta3");
-			$acertos = 0;
+			function tirarAcentos($string){
+				return preg_replace(array("/(á|à|ã|â|ä)/","/(Á|À|Ã|Â|Ä)/","/(é|è|ê|ë)/","/(É|È|Ê|Ë)/","/(í|ì|î|ï)/","/(Í|Ì|Î|Ï)/","/(ó|ò|õ|ô|ö)/","/(Ó|Ò|Õ|Ô|Ö)/","/(ú|ù|û|ü)/","/(Ú|Ù|Û|Ü)/","/(ñ)/","/(Ñ)/"),explode(" ","a A e E i I o O u U n N"),$string);
+			}			
 
-			if (in_array($resposta1, $keys))
+			$resposta1 = tirarAcentos($_POST["resposta1"]);
+			$resposta2 = tirarAcentos($_POST["resposta2"]);
+			$resposta3 = tirarAcentos($_POST["resposta3"]);
+
+			//verificacao das respostas e icrementa o número de acertos
+			if (strtolower($resposta1) == $keyresposta1)
 				$acertos++;
-			if (in_array($resposta2, $keys))
+			if (strtolower($resposta2) == $keyresposta2)
 				$acertos++;
-			if (in_array($resposta3, $keys))
+			if (strtolower($resposta3) == $keyresposta3)
 				$acertos++;	
 
+			//Caso acertou todas as respostas
+			// 	Oculta o quiz e mostra o novo formulário para gerar o certificado
 			if ($acertos == 3) {
 				?>
 				<script>
-					document.querySelector("#contact_form_quiz").style.display = "none";					
+					document.querySelector("#contact_form_quiz").style.display = "none";
+					location.href = "#elementor-action%3Aaction%3Dpopup%3Aopen%26settings%3DeyJpZCI6IjE1NjkiLCJ0b2dnbGUiOmZhbHNlfQ%3D%3D";								
 				</script>
 
 				<form class="form-horizontal" action="" method="post"  id="contact_form_certicate">
 					<fieldset>
-						<center>
-							<h1>Gere seu certificado de Conclusão</h1>
-						</center>
 						<p>&nbsp;</p>
 						<div class="form-group">
 							<label class="col-md-4 control-label">Nome</label>  
 							<div class="col-md-4 inputGroupContainer">
 								<div class="input-group">
 									<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-									<input  name="nome" placeholder="Nome completo" class="form-control"  type="text" maxlength="33">
+									<input  name="nome" placeholder="Nome completo" class="form-control"  type="text" maxlength="40" required>
 								</div>
 							</div>
 						</div>
@@ -105,7 +111,7 @@
 							<div class="col-md-4 inputGroupContainer">
 								<div class="input-group">
 									<span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
-									<input name="email" placeholder="E-Mail" class="form-control"  type="text">
+									<input name="email" placeholder="E-Mail" class="form-control"  type="text" required>
 								</div>
 							</div>
 						</div>
@@ -120,38 +126,43 @@
 					<div id="msg_contact_certificate"></div>
 				</form>
 				<?php 
-
-			}else{
-				echo "Infelizmente você não acertou a quantidade suficiente para o certificado =(";
-			}		
-
-
-		};
+			}
+			//Caso não acerte as 3 respostas
+			// oculta o formulário e mostra a mensagem
+			else{
+				?>
+				<script>
+					document.querySelector("#contact_form_quiz").style.display = "none";
+					location.href = "#elementor-action%3Aaction%3Dpopup%3Aopen%26settings%3DeyJpZCI6IjE1NjkiLCJ0b2dnbGUiOmZhbHNlfQ%3D%3D";						
+				</script>
+				<p><h3>Infelizmente as palavras estão incorretas =(</h3><br> 
+					<span style="font-size: 18px">Mas vamos lá, <b>reassista as aulas</b> e tente novamente.</span></p>
+					<?php 
+				}		
+			};
 
 		// condicao clique no botão de gerar certificado
-		if (isset($_POST['btn-certificado'])){ 
+			if (isset($_POST['btn-certificado'])){ 
 
 			// ----- Variaveis do formulário
-			$nome = strtoupper($_POST['nome']);
-			$email = $_POST['email'];
+				$nome = strtoupper($_POST['nome']);
+				$email = $_POST['email'];
+
 			/** 
 			/ ----- Nome do arquivo que será gerado
-			/ ----- Estrutura: username do email + data atual (zero não precede qualquer número)
+			/ ----- Estrutura: username do email + segundos desde a Era Unix
 			/ ----- Ex.: 
-			/		Entrada: email = user@email.com | data = 20/08/2020
-			/		Saída: user2082020.pdf
+			/		Entrada: email = user@email.com 
+			/		Saída: user1055901520.pdf
 			 **/
 			$first_name_certificate =  strstr($email, '@', true);
 			$today = getdate();
-			echo "day: " . $today['mday'];
-			echo "month: " . $today['mon'];
-			echo "year: " . $today['year'];
-			$name_complete_certificate = $first_name_certificate .  $today['mday'] . $today['mon'] . $today['year'];
+			$name_complete_certificate = $first_name_certificate .  $today["0"];
 
 
 			$pdf = new AlphaPDF();
 
-			// Orientação Landing Page ///
+			// Orientação Landing Page 
 			$pdf->AddPage('L');
 
 			$pdf->SetLineWidth(1.5);
@@ -163,41 +174,41 @@
 			// opacidade total
 			$pdf->SetAlpha(1);
 
-
-
 			// Mostrar o nome
 			$pdf->SetFont('Arial', '', 30); // Tipo de fonte e tamanho
-			$pdf->SetXY(20,60); //Parte chata onde tem que ficar ajustando a posição X e Y
+			$pdf->SetTextColor(158,146,123); //Cor da fonte
+			$pdf->SetXY(19,62); //Parte chata onde tem que ficar ajustando a posição X e Y
 			$pdf->MultiCell(265, 10, $nome, '', 'C', 0); // Tamanho width e height e posição
 
-				// Mostrar o corpo
-				//$pdf->SetFont('Arial', '', 15); // Tipo de fonte e tamanho
-				//$pdf->SetXY(20,110); //Parte chata onde tem que ficar ajustando a posição X e Y
-				//$pdf->MultiCell(265, 10, $texto2, '', 'C', 0); // Tamanho width e height e posição
+			// Dia
+			$pdf->SetFont('Arial', 'B', 17); // Tipo de fonte e tamanho
+			$pdf->SetTextColor(158,146,123); //Cor da fonte
+			$pdf->SetXY(37,123); //Parte chata onde tem que ficar ajustando a posição X e Y
+			$pdf->MultiCell(165, 10, $today['mday'], '', 'C', 0); // Tamanho width e height e posição
 
-				// Mostrar a data no final
-				//$pdf->SetFont('Arial', '', 15); // Tipo de fonte e tamanho
-				//$pdf->SetXY(32,172); //Parte chata onde tem que ficar ajustando a posição X e Y
-				//$pdf->MultiCell(165, 10, $texto3, '', 'L', 0); // Tamanho width e height e posição
 
+			// retorna o documento como string
 			$pdfdoc = $pdf->Output('', 'S');
 
 
-				// ******** Agora vai enviar o e-mail pro usuário contendo o anexo
-				// ******** e também mostrar na tela para caso o e-mail não chegar
-
+			//  enviar o e-mail pro usuário contendo o anexo
+			// e também mostrar na tela para caso o e-mail não chegar
 			$subject = 'Seu Certificado do Workshop';
-			$messageBody = "Olá $nome<br><br>É com grande prazer que entregamos o seu certificado.<br>Ele está em anexo nesse e-mail.<br><br>Atenciosamente,<br>Lincoln Borges<br>";
+			$messageBody = "Olá $nome<br><br>É com grande prazer que entregamos o seu certificado.<br>Ele está em anexo nesse e-mail.<br><br>Atenciosamente,<br>Catlen Guerra<br>";
 
-			$certificado= "/var/www/certificados/$name_complete_certificate.pdf"; //atribui a variável $certificado com o caminho e o nome do arquivo que será salvo (vai usar o CPF digitado pelo usuário como nome de arquivo)
-			$pdf->Output($certificado,'F'); //Salva o certificado no servidor (verifique se a pasta "arquivos" tem a permissão necessária)
-				//$pdf->Output(); // Mostrar o certificado na tela do navegador
+			$certificado= "/var/www/certificados/$name_complete_certificate.pdf"; //atribui a variável $certificado com o caminho e o nome do arquivo que será salvo 
 
+			$pdf->Output($certificado,'F'); //Salva o certificado no servidor (verifique se a pasta "certificados" tem a permissão necessária)
+			
+			//$pdf->Output(); // Mostrar o certificado na tela do navegador
+
+			//gera a url para o certificado gerado
 			$urlDownload =  'http://' . $_SERVER['HTTP_HOST'] . '/certificados/' .  $name_complete_certificate . '.pdf';
 
+			//envia o email com o certificado em anexo e faz o download automaticamente
 			try{
 				$mail = new PHPMailer();
-				$mail->SetFrom("certificado@sweetleads.com.br", "Certificado");
+				$mail->SetFrom("contato@catlenguerra.com.br", "Catlen Guerra");
 				$mail->Subject    = $subject;
 				$mail->MsgHTML(utf8_decode($messageBody));
 				$mail->AddAddress($email); 
@@ -206,13 +217,18 @@
 				$mail->Send()
 
 				?>
+				<style>#contact_form_quiz{display:none !important;}#contact_form_certicate{display: none !important;}</style>
 				<div class="sent-contact">
-					<p>&#10004; Mensagem enviada com sucesso. Te responderemos em breve.</p>
-				</div>
+					<p><b style="font-size: 28px">PARABÉNS!</b><br><br>
+						Agora você possui o certificado de conclusão do curso <br><br>
+						<span style="font-size: 18px">Arte das Cores – Colorimetria Avançada para Maquiadores Online</span><br><br>
 
-				<script language="javascript">
-					window.open('<?php echo $urlDownload; ?>','_blank');
-				</script>
+						<a href="<?php  echo $urlDownload;?>" style="border: 1px solid;border-radius: 5px;padding: 5px">DOWNLOAD DO CERTIFICADO</a><br><br>
+
+						Também enviamos o certificado para seu email, confira lá! =D
+						
+					</p>
+				</div>			
 				<?php 
 
 			}
@@ -224,8 +240,5 @@
 
 		}
 
-
-
 	}
-
 	add_shortcode('certificado-form','gera_certificado');
